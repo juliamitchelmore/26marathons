ddf5360b5fe7788ab66b80224fbbe4375b7ddd08
Julia Mitchelmore <julia@juliamitchelmore.com>
2017-02-06 19:21:50 +0000

    end of heart foundation fundraising

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(function() {
   });
 
   /*get user country*/
-  $.getJSON('http://freegeoip.net/json/', function(result) {
+/*  $.getJSON('http://freegeoip.net/json/', function(result) {
     if(result.country_code == 'AU') {
       $('.charities.trekstock').remove()
     } else {
@@ -24,7 +24,7 @@ $(function() {
   .fail(function() {
     $('.charities.heart-foundation').remove()
   });
-
+*/
   // get VMG fundraising amount -- can't make call via JS :(
 /*  var trekstockUrl = "https://api.virginmoneygiving.com/fundraisers/v1/account/e59f279d-0ffa-47fc-a25d-706c45668bfc/pages/2?api_key=8m7dr543bmkp66rnaa9trg7y";
   $.get(trekstockUrl, function( data ) {
@@ -35,14 +35,14 @@ $(function() {
     $('.trekstock-supporters').text(trekstockSupporters);
   }, "json");*/
 
-  // get everydayhero fundraising amount
+/*  // get everydayhero fundraising amount
   var hfUrl = "https://everydayhero.com/api/v2/pages/1513079";
   $.get(hfUrl, function( data ) {
     var hfTotal = data.page.amount.cents / 100;
     var hfSupporters = data.page.meta.totals.total_donations;
     $('.heart-foundation-raised').text('$' + hfTotal.toFixed(2));
     $('.heart-foundation-supporters').text(hfSupporters);
-  }, "json");
+  }, "json");*/
 });
 
 
